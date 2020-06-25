53e10cc48e2f59c634a4ac16276b8e568c1890bf
Adam Sebesta <adam.sebesta5@gmail.com>
2020-06-25 10:36:42 +0200

    app working as intended, stiil needs to be refactoring on styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+        <router-link
+          :key="post.title"
+          v-for="post in posts"
+          class="link"
+          :to="{ name: 'post', params: { id: post.id } }">
+          {{post.id}}. {{ post.title }}
+        </router-link>
+      </aside>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  props: ['id'],
+  data() {
+    return {
+      posts: [],
+      endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+    };
+  },
+
+  created() {
+    this.getAllPosts();
+  },
+
+  methods: {
+    getAllPosts() {
+      axios.get(this.endpoint)
+        .then((response) => {
+          this.posts = response.data;
+        });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    min-height: 90px;
+    border-bottom: 1px solid #42b983;
+    text-align: center;
+    background: #ffffff;
+  }
+  main {
+    display: flex;
+    height: calc(100vh - 90px);
+    max-width: 1200px;
+    margin-top: 90px;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+  }
+  aside {
+    flex: 1 0 30%;
+    height: 100%;
+    overflow-y: auto;
+    width: 30%;
+    padding: 50px 30px;
+    box-sizing: border-box;
+    border-right: 1px solid #42b983;
+  }
+  .content {
+    flex: 1 1 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .link {
+    display: block;
+    text-decoration: none;
+    margin-bottom: 10px;
+    color: #2c3e50;
+  }
+  .link --home {
+    text-transform: uppercase;
+    margin-bottom: 30px;
+  }
+  .link .is-active {
+    color: #42b983;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+/* eslint-disable */
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from './App.vue';
+import Post from './components/Post.vue';
+import Hello from './components/Hello.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Hello,
+    },
+    {
+      path: '/post/:id',
+      name: 'post',
+      component: Post,
+      props: true,
+    },
+  ],
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: (h) => h(App),
+  router,
+});
